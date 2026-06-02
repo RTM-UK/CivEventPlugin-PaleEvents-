e41e8d3dd7586e9b2febec02f4374512baf79614
RTM <raffertymoyle@gmail.com>
2026-06-02 16:27:56 +0100

    Create PlayerListener.java

---
+++ src/main/java/com/example/civevent/listeners/PlayerListener.java
@@ -0,0 +1,35 @@
+package com.raffe.civevent.listeners;
+
+import com.raffe.civevent.service.NationService;
+import com.raffe.civevent.service.SessionService;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public final class PlayerListener implements Listener {
+    private final NationService nationService;
+    private final SessionService sessionService;
+
+    public PlayerListener(NationService nationService, SessionService sessionService) {
+        this.nationService = nationService;
+        this.sessionService = sessionService;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        nationService.applyVisuals(event.getPlayer());
+        sessionService.updateBossBars();
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        sessionService.removePlayer(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onWorldChange(PlayerChangedWorldEvent event) {
+        sessionService.updateBossBars();
+    }
+}
